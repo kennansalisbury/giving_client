b44da7fff5390c9b741ce6301e78792947b2e92e
kennansalisbury <kennansalisbury@gmail.com>
2020-02-17 08:39:10 -0800

    stubbed user login, functionality working to log user in and access user information on the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Login from './Components/Auth/Login'
+import jwtDecode from 'jwt-decode'
 
 function App() {
+
+  // Hooks for storing current user token
+  let [user, setUser] = useState(null)
+  
+  //Check for token on load
+  useEffect(() => {
+    decodeToken(null)
+  }, [])
+
+  //Decode token function
+  const decodeToken = (existingToken) => {
+    //token is either an existing token of current user, or need to grab from localstorage
+    let token = existingToken || localStorage.getItem('userToken')
+    let decoded
+    
+    //if a token exists, decode it 
+    //then check if expired or invalid - if so, set user null
+    //if both valid & not expired, set user to decoded token object
+    if(token) {
+      decoded = jwtDecode(token)
+      if (!decoded || (Date.now() > decoded.exp * 1000 )) {
+        setUser(null)
+      } else {
+        setUser(decoded)
+      }
+    //if token does not exist, set user to null
+    } else {
+        setUser(null)
+    }
+  }
+
+  //Functino to update the user state at the app level from other components when a user logs in, signs up or logs out
+  const updateUser = (newToken) => {
+    //if there is a new token, store it and decode it (which will set the user to this new info)
+    if(newToken) {
+      localStorage.setItem('userToken', newToken)
+      decodeToken(newToken)
+    } else {
+      //if no token, user is null
+      setUser(null)
+    }
+  }
+
   return (
     <div className="App">
-      <Login />
+      <Login user={user} updateUser={updateUser} />
     </div>
   );
 }
--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,18 +1,23 @@
 import React, {useState} from 'react'
+import {Redirect} from 'react-router-dom'
 
 const Login = props => {
 
+    //set hooks for login form
     let [email, setEmail] = useState('')
     let [password, setPassword] = useState('')
+    let [message, setMessage] = useState('')
     
     const handleSubmit = (e) => {
         e.preventDefault()
-
+        
+        //data to post to login
         let data = {
             email,
             password
         }
 
+        //fetch to post login and return token
         fetch(`${process.env.REACT_APP_SERVER_URL}/auth/login`, {
             method: 'POST',
             body: JSON.stringify(data),
@@ -24,11 +29,26 @@ const Login = props => {
             response.json()
             .then(result => {
                 console.log('Response', response, 'Result', result)
+                //if response.ok = true, updateUser(result.token)
+                if(response.ok) {
+                    props.updateUser(result.token)
+                } else {
+                    //else show the error in a message on the page
+                    setMessage(`${response.status} ${response.statusText}: ${result.message}`)
+                }
             })
         })
         .catch(err => {
             console.log(err)
         })
+    }
+
+    //if there is a user, redirect to home
+    if(props.user) {
+        // return <Redirect to="/Home"/>
+        return (
+            <h1>User logged in: {props.user.firstname} {props.user.lastname}, {props.user.email}</h1>
+        )
     }
     
     return (
@@ -38,6 +58,7 @@ const Login = props => {
             <input type="text" name="password" onChange={e => setPassword(e.currentTarget.value)}/>
             <button type="submit">Submit</button>
            </form>
+           <p>{message}</p>
         </div>
     )
 }
